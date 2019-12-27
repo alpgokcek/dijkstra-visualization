a4663b710fcfcd37d3c008f2b97d6024fb37a4eb
erdal <erdalsidal99@gmail.com>
2019-12-27 22:24:11 +0300

    time plot changed

--- report/matlab/graphs.m
+++ report/matlab/graphs.m
@@ -1,5 +1,10 @@
 N = [10, 50, 100, 200, 500, 1000];
-theoritical_times = [35, 290.52, 692, 1650, 625000, 17000];
+theoritical_times = [35, 290, 692, 1650, 4725, 11500] .* 0.00001;
 actual_times = [0.00012, 0.000382, 0.00084, 0.0021, 0.00977, 0.0351];
-plot(N, times, 'r', 'linewidth', 2)
+
+figure
+plot(N, actual_times, 'r', 'linewidth', 2);
+hold on
+plot(N, theoritical_times, 'g', 'linewidth', 2);
+legend('Observed Time','Theoretical Time');
 xlabel('|V| (# of vertices)'); ylabel('Time (seconds)')
